47abd5c78b213f83a9a31e63bab12ee3446d8116
textic <danielbienchido@gmail.com>
2025-12-09 00:58:38 -0300

    feat: integrate Rofi with dynamic theming via Wallust, enhance wallpaper selector script, and update color configurations for Kitty and Hyprland.

--- .config/custom-scripts/wallpaper-selector.sh
+++ .config/custom-scripts/wallpaper-selector.sh
@@ -2,8 +2,18 @@
 
 WALLPAPER_DIR="$HOME/.config/wallpapers/"
 LINK_PATH="$HOME/.current_wallpaper"
+ROFI_THEME="$HOME/.config/rofi/themes/wallpaper-select.rasi"
 
-SELECTION=$(ls "$WALLPAPER_DIR" | rofi -dmenu -p " 🖼️ Wallpapers " -theme-str 'window {width: 400px;}')
+list_wallpapers() {
+    cd "$WALLPAPER_DIR"
+    for file in *; do
+        if [[ "$file" =~ \.(jpg|jpeg|png|gif|webp)$ ]]; then
+            echo -en "$file\0icon\x1f$WALLPAPER_DIR/$file\n"
+        fi
+    done
+}
+
+SELECTION=$(list_wallpapers | rofi -dmenu -theme "$ROFI_THEME")
 
 if [ -n "$SELECTION" ]; then
     FULL_PATH="$WALLPAPER_DIR/$SELECTION"
@@ -12,7 +22,9 @@ if [ -n "$SELECTION" ]; then
         ln -sf "$FULL_PATH" "$LINK_PATH"
         awww img "$FULL_PATH" --transition-type grow --transition-pos 0.5,0.5 --transition-fps 60 --transition-duration 2
         wallust run "$FULL_PATH"
-        notify-send "Wallpaper and Lockscreen updated" "$SELECTION"
+	killall -SIGUSR2 waybar
+
+        notify-send "Wallpaper updated" "$SELECTION"
     else
         notify-send "Error" "Selected image not found."
     fi
