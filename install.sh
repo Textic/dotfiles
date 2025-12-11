dc8941b2b150ae879d3445847227a433d7942eff
textic <danielbienchido@gmail.com>
2025-12-11 01:30:37 -0300

    feat: enable all modular installation scripts in install.sh

--- install.sh
+++ install.sh
@@ -7,15 +7,15 @@ source ./scripts/00_utils.sh
 echo_msg "Starting modular installation..."
 
 # Run scripts one by one
-#source ./scripts/01_yay.sh
-#source ./scripts/02_packages.sh
-#source ./scripts/03_sddm.sh
-#source ./scripts/04_dotfiles.sh
-#source ./scripts/05_wallpapers.sh
+source ./scripts/01_yay.sh
+source ./scripts/02_packages.sh
+source ./scripts/03_sddm.sh
+source ./scripts/04_dotfiles.sh
+source ./scripts/05_wallpapers.sh
 source ./scripts/06_grub_themes.sh
-#source ./scripts/07_zsh.sh
-#source ./scripts/08_yazi.sh
-#source ./scripts/09_fzf.sh
-#source ./scripts/99_final.sh
+source ./scripts/07_zsh.sh
+source ./scripts/08_yazi.sh
+source ./scripts/09_fzf.sh
+source ./scripts/99_final.sh
 
 echo_msg "All modules executed successfully!"
